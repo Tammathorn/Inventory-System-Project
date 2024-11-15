add132d9ae0554f8a418e2ed83b5b0e6a8f33a5f
kittipichcha <104352517+kittipichcha@users.noreply.github.com>
2024-11-16 00:28:47 +0700

    change the format of the file

---
+++ .history/src/main_20241116001935.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int show_list_product();
+
+int main() {
+
+    char *dataPath = "data\test.csv";
+    FILE *data = fopen(dataPath, "r"); // this is an example
+
+    // -- For login system --
+    
+    int choice;
+    printf("What is your role : ");
+    scanf("%d", &choice);
+    
+    switch(choice) {
+        case 1:
+            printf("Welcome customer!\n");
+            break;
+
+        case 2:
+            printf("Welcome owner!\n");
+            break;
+    }
+
+    // ---------------
+
+    // -- For show list of data --
+    show_list_product(dataPath);
+    return 0;
+}
+
+// function to read and print all the product
+int show_list_product(char *path) {
+    
+    FILE *file = fopen(path, "r");
+    
+
+    if ( (file != NULL) ) {
+
+        char *name;
+        int quantity;
+        float price;
+
+        while ( !feof(file) ) {
+            fscanf(file, "%10s%d%f", name, &quantity, &price);
+            printf("%d %f\n", quantity, price);
+        }
+
+    }
+}
