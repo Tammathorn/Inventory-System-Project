425a4b51d287e4112e0d126e5bc5eebff64da632
kittipichcha <104352517+kittipichcha@users.noreply.github.com>
2024-12-09 22:31:18 +0700

    Add a bit UX writing

---
+++ .history/src/inventory_20241209222532.h
@@ -0,0 +1,28 @@
+#ifndef _INVENTORY_H_
+#define _INVENTORY_H_
+
+extern char *data_path;
+extern char *basket_path;
+
+int customer_system();
+int show_list_product();
+int login_system();
+int editFile();
+int login_system();
+
+// function from other script
+int search();
+int show_list_product();
+int show_list_product_struct();
+
+#define max_char 10
+#define max_product 100
+
+struct file_data {
+    char name[max_char];
+    char type[max_char];
+    int quantity;
+    float price;
+}; 
+
+#endif
--- .history/src/main_20241209213408.c
+++ .history/src/main_20241209213408.c
@@ -17,6 +17,7 @@ int main() {
     int choice;
 
     login_system();
+    // ---------------
     
     return 0;
     
