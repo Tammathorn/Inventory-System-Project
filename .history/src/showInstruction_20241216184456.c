79f89247c9d9b58c210980394e3a8b84e584d42c
kittipichcha <104352517+kittipichcha@users.noreply.github.com>
2024-12-16 19:02:39 +0700

    UX writing

---
+++ .history/src/showInstruction_20241216184456.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int showInstruction() {
+    printf("----------------------------------\n");
+    printf("Press 1 to search\n");
+    printf("Press 2 to find coupon\n");
+    printf("Press 3 to keep coupon\n");
+    printf("Press 4 to show list of current basket\n");
+    printf("Press 5 to apply the coupon\n");
+    printf("Press 6 yo purchase\n");
+    printf("Press 7 to exit\n");
+    printf("Press 6 to purchase\n");
+    printf("----------------------------------\n");
+}
