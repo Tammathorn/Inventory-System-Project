bfa52325590b09caf619ff0824e773c799149adf
Tammathorn <152457560+Tammathorn@users.noreply.github.com>
2024-12-17 13:27:02 +0700

    Update inventory.h

--- src/inventory.h
+++ src/inventory.h
@@ -1,28 +1,45 @@
-#ifndef _INVENTORY_H_
-#define _INVENTORY_H_
+#ifndef INVENTORY_H
+#define INVENTORY_H
 
-extern char *data_path;
-extern char *basket_path;
+// Constants
+#define MAX_PRODUCTS 100
+#define MAX_CHAR 10
+#define MAX_PRODUCT 100
 
-int customer_system();
-int show_list_product();
-int login_system();
-int editFile();
-int login_system();
-
-// function from other script
-int search();
-int show_list_product();
-int show_list_product_struct();
-
-#define max_char 10
-#define max_product 100
-
-struct file_data {
-    char name[max_char];
-    char type[max_char];
+// Product Structure (from inventory1.h)
+typedef struct {
+    int id;
+    char name[50];
     int quantity;
     float price;
-}; 
+} product_t;
 
-#endif
+// File Data Structure (from inventory2.h)
+struct file_data {
+    char name[MAX_CHAR];
+    char type[MAX_CHAR];
+    int quantity;
+    float price;
+};
+
+// External variables (from inventory2.h)
+extern char *data_path;
+extern char *basket_path;
+extern char *coupon_path; 
+
+// Function Prototypes (combined from both headers)
+void create_product();
+void view_products();
+void edit_product();
+void delete_product();
+void save_to_file(const char *file_path);
+void load_data_from_file(const char *file_path);
+void show_list_product(const char *file_path);
+
+// Function prototypes from inventory2.h
+void customer_system();
+void editFile();
+void search();
+void show_list_product_struct();
+
+#endif // INVENTORY_H
